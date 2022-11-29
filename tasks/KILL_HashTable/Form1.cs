c4ec83dd93e56314e4dc0c64b259eb891b330210
AlexTing <adr.tsiklov@mail.ru>
2022-11-29 19:26:24 +0300

    add task-hashtable

---
+++ tasks/KILL_HashTable/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+// remove проходит 2 раза - пофиксить
+
+namespace KILL_HashTable {
+	public partial class Form1 : Form {
+
+		private HashTable<string, int> hashTable;
+
+		public Form1() {
+			InitializeComponent();
+			hashTable = new HashTable<string, int>();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			try {
+				string key = textBox1.Text;
+				int value = Int32.Parse(textBox2.Text);
+				hashTable.add(key, value);
+				printListBox();
+			} catch {
+				MessageBox.Show("Wrong input!");
+			}
+		}
+		
+		private void button2_Click(object sender, EventArgs e) {
+
+			string key = textBox3.Text;
+			
+			try {
+				int value = hashTable.find(key);
+				label5.Text = value.ToString();
+			} 
+			catch (KeyDoesNotExistException ex) {
+				label5.Text = ex.Message;
+			}
+		}
+	
+		private void button3_Click(object sender, EventArgs e) {
+			string key = textBox4.Text;
+			
+			try {
+				hashTable.remove(key);
+				printListBox();
+			}
+			catch (KeyDoesNotExistException ex) {
+				label6.Text = ex.Message;
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e) {
+			label7.Text = hashTable.Count.ToString();
+		}
+
+		private void button5_Click(object sender, EventArgs e) {
+			Dictionary<string, int> generatedData = generateStringKeyIntValue(20);
+			foreach(KeyValuePair<string, int> entry in generatedData) {
+				hashTable.add(entry.Key, entry.Value);
+			}
+			printListBox();
+		}
+
+		public Dictionary<string, int> generateStringKeyIntValue(int initialCount) {
+			Dictionary<string, int> generatedData = new Dictionary<string, int>();
+			char[] letters = "qwertyuiopasdfghjklzxcvbnm".ToCharArray();
+			Random rand = new Random();
+
+			for(int i = 0; i < initialCount; i++) {
+				int randomValue = rand.Next(0, 1000);
+				string randomKey = "";
+				
+				for (int j = 1; j <= 5; j++) {
+					int letter_num = rand.Next(0, letters.Length - 1);
+					randomKey += letters[letter_num];
+				}
+				generatedData.Add(randomKey, randomValue);
+			}
+			return generatedData;
+		}
+
+		private void printListBox() {
+			listBox1.Items.Clear();
+			listBox2.Items.Clear();
+
+            if(hashTable.Count != 0) {
+                foreach(var value in hashTable.toList()) {
+                    listBox1.Items.Add(value);
+                }
+            }
+
+			foreach(var indexList in hashTable) {
+				listBox2.Items.Add(indexList);
+			}
+		}
+
+	}
+}
---
+++ tasks/KILL_HashTable/HashTable.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+
+
+namespace KILL_HashTable {
+
+	internal class HashTable<Tkey, Tvalue> {
+
+		private static int Capasity { get { return 10; } }
+		List<HashTableElement<Tkey, Tvalue>>[] hashTable = new List<HashTableElement<Tkey, Tvalue>> [Capasity];
+		private int count;
+		public  int Count { get { return count; }}
+
+		public void add(Tkey key, Tvalue value) {
+			int hashCode = getHashCode(key);
+			
+			if(hashTable[hashCode] == null) {
+				hashTable[hashCode] = new List<HashTableElement<Tkey, Tvalue>>();
+			}
+			else {
+				foreach(var hashTableElement in hashTable[hashCode]) {
+					if(hashTableElement.key.Equals(key)) {
+						hashTableElement.value = value;
+						return;
+					}
+				}
+			}
+			hashTable[hashCode].Add(new HashTableElement<Tkey, Tvalue>(key, value));
+			count++;
+		}
+
+		public Tvalue find(Tkey key) {
+		    int hashCode = getHashCode(key);
+			List<HashTableElement<Tkey, Tvalue>> listWithThisHashCode = findListByHash(hashCode);
+			return findElementInList(listWithThisHashCode, key).value;
+		}
+
+		public void remove(Tkey key) {
+			int hashCode = getHashCode(key);
+			if(findListByHash(hashCode).RemoveAll(elem => elem.key.Equals(key)) != 0) {
+				count--;
+			}
+		}
+
+		public List<string> toList() {
+			List<string> outputList = new List<string>();
+
+			foreach(var list in hashTable) {
+				if(list != null) {
+					foreach(var element in list) {
+						outputList.Add(element.key.ToString() + ": " + element.value.ToString());
+					}
+				}
+			}
+			return outputList;
+		}
+
+		private int getHashCode(Tkey key) {
+			return Math.Abs(key.GetHashCode() % Capasity);
+		}
+
+		private List<HashTableElement<Tkey, Tvalue>> findListByHash(int hashCode) {
+			List<HashTableElement<Tkey, Tvalue>> listWithThisHashCode = hashTable[hashCode];
+			
+			if(listWithThisHashCode == null) {
+				throw new KeyDoesNotExistException("This key does not exist!");
+			}
+			return listWithThisHashCode;
+		}
+
+		private HashTableElement<Tkey, Tvalue> findElementInList(List<HashTableElement<Tkey, Tvalue>> listWithThisHashCode, Tkey key) {
+			foreach(var hashTableElement in listWithThisHashCode) {
+				if(hashTableElement.key.Equals(key)) {
+					return hashTableElement;
+				}
+			}
+			throw new KeyDoesNotExistException("This key does not exist!");
+		}
+
+		private class HashTableElement<Tkey, Tvalue> {
+			
+			public Tkey key;
+			public Tvalue value;
+
+			public HashTableElement(Tkey key, Tvalue value) {
+				this.key = key;
+				this.value = value;
+			}
+		}
+
+		 private class ListPrototypeEnumerator : IEnumerator {
+			List<HashTableElement<Tkey, Tvalue>>[] hashTable;
+			private int capasity;
+			private int indexInHashTable;
+			private int indexInList = -1;
+
+            public ListPrototypeEnumerator(List<HashTableElement<Tkey, Tvalue>>[] hashTable, int capasity) {
+                this.hashTable = hashTable;
+				this.capasity = capasity;
+            }
+
+            public object Current {
+                get {
+                    return indexInHashTable.ToString() + ": " + hashTable[indexInHashTable][indexInList].value.ToString();
+                }
+            }
+
+            public bool MoveNext() {
+				while(indexInHashTable != capasity) {
+					if(hashTable[indexInHashTable] == null) {
+						indexInHashTable++;
+					}
+					else {
+						if(indexInList + 1 != hashTable[indexInHashTable].Count) {
+							indexInList++;
+							return true;
+						} else {
+							indexInList = 0;
+							indexInHashTable++; 
+						}
+					}
+				}
+                return false;
+
+            }
+            public void Reset() => indexInHashTable = 0;
+        }
+
+        public IEnumerator GetEnumerator() => new ListPrototypeEnumerator(hashTable, Capasity);
+    }
+}
